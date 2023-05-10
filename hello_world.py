27397696cabefeedcafe1ec0b8a983c177ab0392
zjhxmjl <zjhxmjl@gmail.com>
2023-05-10 14:01:51 +0800

    hello world

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File              : hello_world.py
+# Author            : zjhxmjl <zjhxmjl@gmail.com>
+# Date              : 10.05.2023
+# Last Modified Date: 10.05.2023
+# Last Modified By  : zjhxmjl <zjhxmjl@gmail.com>
+print ("Hello the world!")
